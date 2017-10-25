8bd18abf8dea2bf6d4dc42ef9119c4e4cd72f7f0
gonefuture <gonefuture@qq.com>
2017-10-25 22:22:12 +0800

    添加了LoginService和LoginController,:star: 增添了Message信息类，修复了一些bug。:bug:

--- src/main/java/cn/zhku/waterfowl/modules/user/service/LoginService.java
+++ src/main/java/cn/zhku/waterfowl/modules/user/service/LoginService.java
@@ -1,5 +1,9 @@
 package cn.zhku.waterfowl.modules.user.service;
 
+import cn.zhku.waterfowl.pojo.entity.User;
+import cn.zhku.waterfowl.pojo.entity.UserExample;
+import cn.zhku.waterfowl.pojo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,5 +13,32 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LoginService {
+    @Autowired
+    UserMapper userMapper;
 
+
+    /**
+     *  登录服务
+     * @param form  user ,必须参数:phone，password
+     * @return True：成功 False：失败
+     */
+    public boolean login(User form) {
+        UserExample userExample = new UserExample();
+        userExample.or().andPhoneEqualTo(form.getPhone());
+        User user = userMapper.selectByExample(userExample).get(0);
+        if (user.getPassword().equals(form.getPassword()))
+            return Boolean.TRUE;
+        else
+            return Boolean.FALSE;
+    }
+
+    //检验手机号
+    public boolean CheckPhone(User user) {
+        return false;
+    }
+
+
+    public boolean register(User user) {
+        return false;
+    }
 }
