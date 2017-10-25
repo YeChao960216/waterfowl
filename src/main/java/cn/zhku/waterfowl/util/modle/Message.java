038b086d62142976229a3627f2124345dabe49eb
gonefuture <gonefuture@qq.com>
2017-10-25 22:23:22 +0800

    添加了LoginService和LoginController,:star: 增添了Message信息类，修复了一些bug。:bug: 配置文件的些许改动。

--- src/main/java/cn/zhku/waterfowl/util/modle/Message.java
+++ src/main/java/cn/zhku/waterfowl/util/modle/Message.java
@@ -4,27 +4,34 @@ package cn.zhku.waterfowl.util.modle;
  * @author 钱伟健 gonefutre
  * @date 2017/8/1 22:25.
  * @E-mail gonefuture@qq.com
+ *
+ * 说明：状态信息类
  */
 public class Message {
    //状态码
+    private String status;
+    //具体信息
     private String msg;
-    //信息
-    private String info;
     //用来放各种关键的属性，如userId
     private String key;
 
-    public Message() {
+    public Message(String status, String msg) {
+        this.status = status;
+        this.msg = msg;
     }
 
-    public Message(String msg, String info) {
+    public Message(String status, String msg, String key) {
+        this.status = status;
         this.msg = msg;
-        this.info = info;
-    }
-
-    public Message(String msg, String info, String key) {
-        this.msg = msg;
-        this.info = info;
         this.key = key;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String getMsg() {
@@ -33,14 +40,6 @@ public class Message {
 
     public void setMsg(String msg) {
         this.msg = msg;
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
     }
 
     public String getKey() {
@@ -54,8 +53,8 @@ public class Message {
     @Override
     public String toString() {
         return "Message{" +
-                "msg='" + msg + '\'' +
-                ", info='" + info + '\'' +
+                "status='" + status + '\'' +
+                ", msg='" + msg + '\'' +
                 ", key='" + key + '\'' +
                 '}';
     }
