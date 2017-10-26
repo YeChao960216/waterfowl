6ab7018e604701382f2ff70881f177314f1009dd
mumuweijia <984613906@qq.com>
2017-10-26 15:05:41 +0800

    做好了基本的增删改查　　但没有进行调试

--- src/main/java/cn/zhku/waterfowl/modules/epidemic/controller/EpidemicController.java
+++ src/main/java/cn/zhku/waterfowl/modules/epidemic/controller/EpidemicController.java
@@ -1,9 +1,9 @@
 package cn.zhku.waterfowl.modules.epidemic.controller;
 
 import cn.zhku.waterfowl.modules.epidemic.service.EpidemicService;
-import cn.zhku.waterfowl.modules.user.service.UserService;
+
 import cn.zhku.waterfowl.pojo.entity.Epidemic;
-import cn.zhku.waterfowl.pojo.entity.User;
+
 import cn.zhku.waterfowl.util.modle.CommonQo;
 import cn.zhku.waterfowl.web.BaseController;
 import com.github.pagehelper.PageHelper;
@@ -12,11 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import cn.zhku.waterfowl.modules.user.service.UserService;
-import cn.zhku.waterfowl.pojo.entity.User;
 import cn.zhku.waterfowl.util.modle.CommonQo;
 import cn.zhku.waterfowl.web.BaseController;
-
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,32 +21,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.List;
 import java.util.UUID;
-
-import java.util.List;
-import java.util.UUID;
-
-public class EpidemicController {package cn.zhku.waterfowl.modules.epidemic.controller;
-
-
 
 /**
  * @author  : 林伟佳 gonefutre
- *  date    : 2017-10-24
+ *  date    : 2017-10-26
  *  E-mail  : 984613906@qq.com
  */
-
     /**
      * 对免疫epidemic状态操作的controller,继承BaseController以便使用全局变量和参数绑定
      */
     @Controller
-    @RequestMapping("{adminPath}/user/")
-    public class epidemicController extends BaseController {
+    @RequestMapping("{epidemicPath}")
+    public class EpidemicController extends BaseController {
+        @Autowired
+        EpidemicService epidemicService;
         @Autowired
         //增加用户
-
         /** 增加免疫记录
          *
          * @param  epidemic 实体类的各种字段，如
@@ -57,14 +46,11 @@ public class EpidemicController {package cn.zhku.waterfowl.modules.epidemic.cont
          * @throws Exception sql
          */
         @ResponseBody
-        @RequestMapping("Epidemic")
+        @RequestMapping("epidemic")
         public int addEpidemic(Epidemic epidemic) throws Exception {
-           Epidemic.setId(UUID.randomUUID().toString().replace("-","").toUpperCase());   //用32位大小的UUID来设置用户id
-            return EpidemicService.add(epidemic);
+            epidemic.setId(UUID.randomUUID().toString().replace("-","").toUpperCase());   //用32位大小的UUID来设置用户id
+            return epidemicService.add(epidemic);
         }
-
-        //删除免疫记录
-
         /** 根据记录表id删除对象
          *
          * @param id   只需id字段
@@ -76,12 +62,8 @@ public class EpidemicController {package cn.zhku.waterfowl.modules.epidemic.cont
         public int deleteEpidemic(@PathVariable String id) throws Exception {
             Epidemic epidemic = new Epidemic();
             epidemic.setId(id);
-            return EpidemicService.delete(epidemic);
+            return epidemicService.delete(epidemic);
         }
-
-
-        //修改免疫记录
-
         /** 根据id修改免疫记录
          *
          * @param   id 包括记录表id的各个Epidemic实体类字段
@@ -92,13 +74,10 @@ public class EpidemicController {package cn.zhku.waterfowl.modules.epidemic.cont
         @RequestMapping("edit/{id}")
         public int editEpidemic(@PathVariable String id,Epidemic epidemic) throws Exception {
             epidemic.setId(id);
-            return EpidemicService.update(epidemic);
+            return epidemicService.update(epidemic);
         }
 
-        //展示用户
-
         /** 根据id展示免疫信息
-         *
          * @param id   只需记录表id
          * @return Epidemic实体
          * @throws Exception    sql
@@ -106,28 +85,26 @@ public class EpidemicController {package cn.zhku.waterfowl.modules.epidemic.cont
         @ResponseBody
         @RequestMapping("show/{id}")
         public Epidemic showEpidemic(@PathVariable String id) throws Exception {
-            return EpidemicService.get(id);
+            return epidemicService.get(id);
         }
-        //展示所有免疫记录表
-
+        //分页展示免疫记录表
         /**
-         *  根据多个添加添加展示一列用户 => 多条件查询分页
+         *  根据多个条件展示一列用户 => 多条件查询分页
          * @param epidemic   epidemic实体的各个字段
          * @param commonQo   通用查询类，拥有pageSize,
          * @return  PageInfo<Epidemic> 一个带有List<User>的pageBean
          * @throws Exception    sql
          */
         @ResponseBody
-        @RequestMapping("list")
+        @RequestMapping("showFindList")
         public PageInfo<Epidemic> listEpidemic(Epidemic epidemic, CommonQo commonQo) throws Exception {
             //  设置页码，页面大小，排序方式,此处的sql相当于 limit pageNum ,pageSize orderBy id desc
             PageHelper.startPage(commonQo.getPageNum(), commonQo.getPageSize(), "id desc");
             //  通过服务层获取查询后的用户列表
-            List<Epidemic> epidemicList =  EpidemicService.findList(epidemic);
+            List<Epidemic> epidemicList =  epidemicService.findList(epidemic);
             //  返回 pageBean
             return new PageInfo<Epidemic>(epidemicList);
         }
 
     }
 
-}
--- src/main/java/cn/zhku/waterfowl/modules/epidemic/service/EpidemicService.java
+++ src/main/java/cn/zhku/waterfowl/modules/epidemic/service/EpidemicService.java
@@ -1,51 +1,99 @@
 package cn.zhku.waterfowl.modules.epidemic.service;
-
 import cn.zhku.waterfowl.modules.epidemic.service.EpidemicService;
-import cn.zhku.waterfowl.pojo.entity.Epidemic;
 import cn.zhku.waterfowl.pojo.entity.Epidemic;
 import cn.zhku.waterfowl.pojo.entity.EpidemicExample;
 import cn.zhku.waterfowl.pojo.mapper.EpidemicMapper;
 import cn.zhku.waterfowl.util.interfaceUtils.IBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import java.util.List;
 
 public class EpidemicService  implements IBaseService<Epidemic>  {
     @Autowired
-    EpidemicService epidemicService;
-    public  int add(Epidemic entity) {
-    }
+    private EpidemicMapper epidemicMapper;
 
-    public  int delete(Epidemic entity) {
-    }
-
-    public  int update(Epidemic entity) {
-
-    }
-
+    @Override
     /**
-     *  根据主键id删除
-     * @param id
-     *            主键ID
-     * @return 状态码 1 成功， 0 失败
-     * @throws Exception sql
+     * 根据epidemic实体插入
      */
-    public  int delete(Epidemic entity) {
-        return EpidemicMapper.deleteByPrimaryKey(entity);
+    public int add(Epidemic entity) throws Exception {
+        return epidemicMapper.insert(entity);
     }
+
+    @Override
     /**
-     *  根据主键返回 epidemic
-     * @param id
-     *            主键ID
-     * @return user
-     * @throws Exception sql
+     * 根据epidemic实体更新
      */
-    public  Epidemic get(String id) {
-        return EpidemicMapper.selectByPrimaryKey(id);
+    public int update(Epidemic entity) throws Exception {
+        return epidemicMapper.updateByPrimaryKeySelective(entity);
     }
 
-    public  List<Epidemic> findList(Epidemic entity) {
+    @Override
+    /**
+     *根据epidemic实体类id删除
+     */
+    public int delete(Epidemic entity) throws Exception {
+        return epidemicMapper.deleteByPrimaryKey(entity.getId());
+    }
 
+    @Override
+    /**
+     * 根据id得到epdemic实体类
+     */
+    public Epidemic get(String id) throws Exception {
+        return epidemicMapper.selectByPrimaryKey(id);
+    }
 
+    @Override
+    /**
+     * 根据epdemic实体类的id查询
+     */
+    public Epidemic get(Epidemic entity) throws Exception {
+        EpidemicExample epidemicExample =new EpidemicExample();
+        epidemicExample.or().andIdBatchEqualTo(entity.getId());
+        if (epidemicExample!=null) {
+            return epidemicMapper.selectByExample(epidemicExample).get(0);
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    /**
+     * 根据epidemic实体多条件查询，不分页
+     */
+    public List<Epidemic> findList(Epidemic entity) throws Exception {
+        EpidemicExample epidemicExample =new EpidemicExample();
+        EpidemicExample.Criteria criteria = epidemicExample.createCriteria();
+        //日期
+        if (entity.getDate()!=null)
+            criteria.andDateEqualTo(entity.getDate());
+        //疾病
+        if (entity.getDiseaes()!=null)
+            criteria.andDiseaesLike("%"+entity.getDiseaes()+"%");
+        //给药方式
+        if (entity.getProcessingMode()!=null)
+            criteria.andProcessingModeLike("%"+entity.getProcessingMode()+"%");
+        //负责人编号
+        if (entity.getIdCharge()!=null)
+            criteria.andIdChargeEqualTo(entity.getIdCharge());
+        //批次编号
+        if (entity.getIdBatch()!=null)
+            criteria.andIdBatchEqualTo(entity.getIdBatch());
+        //库存编号
+        if (entity.getInventoryId()!=null);
+            criteria.andIdNotEqualTo(entity.getInventoryId());
+        //记录者编号
+        if (entity.getIdRecorder()!=null)
+            criteria.andIdRecorderEqualTo(entity.getIdRecorder());
+        //备注
+        if (entity.getRemark()!=null)
+            criteria.andRemarkLike("%"+entity.getRemark()+"%");
+        //处理方式
+        if (entity.getProcessingMode()!=null)
+            criteria.andProcessingModeLike("%"+entity.getProcessingMode()+"%");
+        //免疫，疾病标志
+        if (entity.getSign()!=null)
+            criteria.andSignEqualTo(entity.getSign());
+        return epidemicMapper.selectByExample(epidemicExample);
     }
 }
