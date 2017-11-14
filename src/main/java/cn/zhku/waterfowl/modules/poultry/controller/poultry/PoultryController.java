77b1a2d2108e09f0ee17451542be8d01f4c5cb25
gonefuture <gonefuture@qq.com>
2017-11-14 23:23:07 +0800

    添加了poultry和material模块

---
+++ src/main/java/cn/zhku/waterfowl/modules/poultry/controller/poultry/PoultryController.java
@@ -0,0 +1,9 @@
+package cn.zhku.waterfowl.modules.poultry.controller.poultry;
+
+/**
+ * @author : 钱伟健 gonefuture@qq.com
+ * @version : 2017/11/14 18:21.
+ * 说明：
+ */
+public class PoultryController {
+}
---
+++ src/main/java/cn/zhku/waterfowl/util/interfaceUtils/IBaseController.java
@@ -0,0 +1,11 @@
+package cn.zhku.waterfowl.util.interfaceUtils;
+
+/**
+ * @author : 钱伟健 gonefuture@qq.com
+ * @version : 2017/11/14 22:36.
+ * 说明：
+ */
+public interface IBaseController {
+
+
+}
