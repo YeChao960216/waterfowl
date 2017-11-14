2b7b998d72aa2834e0bc39567ae6dd7d8c6d76ce
gonefuture <gonefuture@qq.com>
2017-11-14 16:59:31 +0800

    字典接口service忘了加注解@service

--- src/main/java/cn/zhku/waterfowl/modules/dictionary/service/DictionaryService.java
+++ src/main/java/cn/zhku/waterfowl/modules/dictionary/service/DictionaryService.java
@@ -5,6 +5,7 @@ import cn.zhku.waterfowl.pojo.entity.DictionaryExample;
 import cn.zhku.waterfowl.pojo.mapper.DictionaryMapper;
 import cn.zhku.waterfowl.util.interfaceUtils.IBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  * @version : 2017/11/13 23:12.
  * 说明：
  */
+@Service
 public class DictionaryService  implements IBaseService<Dictionary> {
 
     @Autowired
