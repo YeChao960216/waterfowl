b02731651bd67bece63d32f3b637d04f9e8cb627
gonefuture <gonefuture@qq.com>
2017-11-15 21:21:50 +0800

    更新了材料表的接口

--- src/main/java/cn/zhku/waterfowl/modules/material/controller/MaterialController.java
+++ src/main/java/cn/zhku/waterfowl/modules/material/controller/MaterialController.java
@@ -1,17 +1,28 @@
 package cn.zhku.waterfowl.modules.material.controller;
 
+import cn.zhku.waterfowl.modules.material.service.MaterialService;
 import cn.zhku.waterfowl.pojo.entity.Material;
 import cn.zhku.waterfowl.util.interfaceUtils.IBaseController;
 import cn.zhku.waterfowl.util.modle.CommonQo;
 import cn.zhku.waterfowl.util.modle.Message;
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : 钱伟健 gonefuture@qq.com
  * @version : 2017/11/14 18:23.
  * 说明：
  */
+@RestController
 public class MaterialController implements IBaseController<Material> {
+    @Autowired
+    MaterialService materialService;
+
+
+
+
     /**
      * 增加一条数据
      *
@@ -21,19 +32,26 @@ public class MaterialController implements IBaseController<Material> {
      */
     @Override
     public Message add(Material entity) throws Exception {
-        return null;
+        if (materialService.add(entity) == 1)
+            return new Message("1","插入材料成功");
+        else
+            return new Message("2","插入材料失败");
+
     }
 
     /**
      * 删除一条记录
      *
-     * @param id 表主键
+     * @param entity id 表主键
      * @return Message
      * 1     * @throws Exception   sql、bean检验
      */
     @Override
-    public Message delete(String id) throws Exception {
-        return null;
+    public Message delete(Material entity) throws Exception {
+        if (materialService.delete(entity) == 1)
+            return new Message("1","成功删除材料");
+        else
+            return new Message("2","删除材料失败");
     }
 
     /**
@@ -43,7 +61,10 @@ public class MaterialController implements IBaseController<Material> {
      */
     @Override
     public Message update(Material entity) throws Exception {
-        return null;
+        if (materialService.update(entity) == 1)
+            return new Message("1","更新材料信息成功");
+        else
+            return new Message("2","更新材料信息失败");
     }
 
     /**
@@ -55,7 +76,7 @@ public class MaterialController implements IBaseController<Material> {
      */
     @Override
     public Material get(String id) throws Exception {
-        return null;
+        return materialService.get(id);
     }
 
     /**
@@ -68,6 +89,8 @@ public class MaterialController implements IBaseController<Material> {
      */
     @Override
     public PageInfo<Material> list(Material entity, CommonQo commonQo) throws Exception {
-        return null;
+        PageHelper.startPage(commonQo.getPageNum(),commonQo.getPageSize(),"date desc");
+
+        return new PageInfo<>(materialService.findList(entity));
     }
 }
