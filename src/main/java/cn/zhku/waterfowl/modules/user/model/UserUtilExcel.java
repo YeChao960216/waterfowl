8ddaa4c3529f34df763fa56ed87b998d46f884ac
gonefuture <gonefuture@qq.com>
2017-11-19 19:53:57 +0800

    更新了字典的接口

---
+++ src/main/java/cn/zhku/waterfowl/modules/user/model/UserUtilExcel.java
@@ -0,0 +1,21 @@
+package cn.zhku.waterfowl.modules.user.model;
+
+import cn.zhku.waterfowl.util.excel.ImportExcelUtils;
+
+/**
+ * @author : 钱伟健 gonefuture@qq.com
+ * @version : 2017/11/18 11:38.
+ * 说明：
+ */
+public class UserUtilExcel extends ImportExcelUtils<UserExcel> {
+    /**
+     * 构造工具类
+     *
+     * @param filepath
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public UserUtilExcel(String filepath) throws InstantiationException, IllegalAccessException {
+        super(filepath);
+    }
+}
