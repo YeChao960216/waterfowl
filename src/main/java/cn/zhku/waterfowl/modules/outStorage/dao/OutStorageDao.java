c7224ea6e4a39373f0879b0ca2061c944149c458
yec12 <yc755909707@foxmail.com>
2017-12-03 15:55:07 +0800

    因数据库改变而做的调整

--- src/main/java/cn/zhku/waterfowl/modules/outStorage/dao/OutStorageDao.java
+++ src/main/java/cn/zhku/waterfowl/modules/outStorage/dao/OutStorageDao.java
@@ -11,4 +11,9 @@ public interface OutStorageDao {
      * */
     @Select("select * from material where name like concat('%',#{name},'%') and type='未过期'")
     List<Material> listMatericalByName(String name);
+//    /*
+//     * 根据type是否为'未过期'和'已过期'
+//     * */
+//    @Select("select * from material where name like concat('%',#{name},'%') and type='未过期'")
+//    List<Material> listMatericalByName(String name);
 }
