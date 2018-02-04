738308a92ce928a06c4398add098290331f2fc25
yec12 <yc755909707@foxmail.com>
2018-02-04 21:53:15 +0800

    去除patch一些无关的方法

---
+++ yechao/src/main/java/cn/zhku/YechaoApplication.java
@@ -0,0 +1,12 @@
+package cn.zhku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YechaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YechaoApplication.class, args);
+	}
+}
